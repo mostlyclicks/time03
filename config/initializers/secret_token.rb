29f7a0d22891c1c54cdf25d35dbe02054887d9cc
Carlos Interone <carlos@mostlyclicks.com>
2014-10-02 10:29:18 -0500

    initial commit w bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Time03::Application.config.secret_token = '7dcd8832a5b30acc7dbe7d83ed24e624a98d3ec4333dfa0c487a4c545aa0e5b89913f2423d74eca6473258fde512c8788b7b2a94bbcc5a9025a3e9b27e7e1234'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Time03::Application.config.session_store :cookie_store, key: '_time03_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Time03::Application.config.session_store :active_record_store
